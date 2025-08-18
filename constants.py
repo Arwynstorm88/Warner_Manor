7d923352e1a8c6d326ae6213fbc78801efa27c4f
Christopher L Hoeppner <hounddog88@gmail.com>
2025-08-17 19:47:20 -0500

    Refactor inventory logic: moves to player class, added constants.py (#3)
    Refactor inventory logic: moves to player class, added constants.py

---
+++ constants.py
@@ -0,0 +1,4 @@
+menu = 'Move Commands: Go North | Go South | Go East | Go West | Exit'
+alt_commands = 'Add to Inventory: Get "item name".\nTo see this again: Help'
+VALID_MOVES = {'North', 'South', 'East', 'West'}
+total_clues = 7
